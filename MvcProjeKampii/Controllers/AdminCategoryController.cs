82240f7bd822145723384f91cf2e7dea828978e0
bedranozcan <bedranozcan.github@gmail.com>
2022-03-30 15:26:23 +0300

    Admin paneli yükleme ve admin paneli üzerinde değişiklikler

---
+++ MvcProjeKampii/Controllers/AdminCategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampii.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        // GET: AdminCategoryController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
