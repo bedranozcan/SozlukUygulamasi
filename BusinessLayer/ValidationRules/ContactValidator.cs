aa087ce8d6d3fe21779122a94257664916eaae1d
bedranozcan <bedranozcan.github@gmail.com>
2022-04-12 23:56:19 +0300

    Contact ve Content Sınıflarının CRUD işlemleri ve arayüz çalışmaları

---
+++ BusinessLayer/ValidationRules/ContactValidator.cs
@@ -0,0 +1,23 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(x => x.UserMail).NotEmpty().WithMessage("Mail adresini boş geçemezsiniz");
+            RuleFor(x => x.Subject).NotEmpty().WithMessage("Konuyu boş geçemezsiniz.");
+            RuleFor(x => x.UserName).NotEmpty().WithMessage("İsim alanını boş bırakamazsınız");
+            RuleFor(x => x.Subject).MinimumLength(3).WithMessage("En az 3 karakter girişi yapabilirsiniz.");
+            RuleFor(x => x.Subject).MinimumLength(50).WithMessage("En fazla 50 karakter girişi yapabilirsiniz.");
+            RuleFor(x => x.UserName).MaximumLength(2).WithMessage("Lütfen en az 2 karakter girişi yapın.");         
+        }
+    }
+}
